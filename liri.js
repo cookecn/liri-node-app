5ec92e783586ce9c7b8bdb17745c1ec9e6e95296
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-03-28 20:26:53 -0500

    initial commit

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new spotify(keys.spotify);
+
+/*
+concert-this;
+spotify-this-song;
+movie-this;
+do-what-it-says;
+*/
+
+/* FOr Bands in Town 
+
+("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+
+*/
